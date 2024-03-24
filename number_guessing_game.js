8e17a56b97d92a01f8f0834bcbc576c8b72a53cf
WA Siddiqui-coder <163776495+WA-code-Github-Account@users.noreply.github.com>
2024-03-24 11:45:05 -0700

    my first commit

---
+++ number_guessing_game.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const ramdomNumber = Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number between 1 - 6:",
+    },
+]);
+if (answers.userGuessNumber === ramdomNumber) {
+    console.log("Congtratulations! you guessed right number");
+}
+else {
+    console.log("You gussed a wrong number");
+}
